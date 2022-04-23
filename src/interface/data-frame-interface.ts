58bec63c77150254ba8c1358c1357744802b8e18
Thagana <samuelmthwa79@gmail>
2022-04-23 17:26:15 +0200

    mailing notification

---
+++ src/interface/data-frame-interface.ts
@@ -0,0 +1,15 @@
+interface DataFrame {
+    id: string;
+    title: string;
+    source: string;
+    author: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    description: string;
+    dateCreated: string;
+    country: string;
+    category: string;
+}
+
+export default DataFrame;
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,45 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import routes from './routers/routers';
-import db from './configs/db.mongodb';
-import newSaveCron from './Jobs/saveNews';
-import loggerMiddleware from './middleware/logger';
+import * as dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import routes from "./routers/routers";
+import db from "./configs/db.mongodb";
+import newSaveCron from "./Jobs/saveNews";
+import loggerMiddleware from "./middleware/logger";
 
 dotenv.config();
 
 if (!process.env.PORT) {
-	process.exit(1);
+  process.exit(1);
 }
-
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('connected', () => console.log('Connect to Mongodb Database'));
-db.on('disconnected', () => console.log('Database disconnected'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("connected", () => console.log("Connect to Mongodb Database"));
+db.on("disconnected", () => console.log("Database disconnected"));
 
 const app = express();
 
-app.use(helmet());
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+    },
+  })
+);
 app.use(cors());
 app.use(express.json());
-app.use(loggerMiddleware)
+app.use(loggerMiddleware);
+
 routes(app);
 
-newSaveCron.start();
+// newSaveCron.start();
 
 app.listen(PORT, () => {
-    console.log(`Application running successfully on ${PORT}`)
-})
+  console.log(`Application running successfully on ${PORT}`);
+});
