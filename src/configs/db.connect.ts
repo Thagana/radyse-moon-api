b9ea7834c71b16056ba5bdc1ce7d9205b8f28717
Thagana <samuelmthwa79@gmail>
2022-08-14 17:46:12 +0200

    minor changes

--- src/configs/db.connect.ts
+++ src/configs/db.connect.ts
@@ -8,8 +8,6 @@ const db = new Sequelize(DATABASE_CONNECTION);
 
 export const SQ = db;
 
-db.authenticate()
-  .then(() => logger.info("Successfully connected to the database"))
-  .catch((error) => logger.error(error));
+db.authenticate().then(() => logger.info("Successfully connected to the database")).catch((error) => logger.error(error));
 
 export default db;
