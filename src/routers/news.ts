76070e2c3bae0dfbca4f9681a65c64b0e2e689d3
Thagana <samuelmthwa79@gmail>
2022-08-21 13:19:23 +0200

    updating the cron job to an url

--- src/routers/news.ts
+++ src/routers/news.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get('/headlines', auth, news.headlines);
 router.post('/search', news.search);
+router.post('fetch-news', news.fetchNews);
 
 export default router;
